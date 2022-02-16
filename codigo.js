fdde6512568fa2d1d7a7389102c1a2a68a06fba8
AndresAzcurra <andresazcurra@gmail.com>
2022-02-16 02:29:16 -0300

    laboratorio

---
+++ codigo.js
@@ -0,0 +1,27 @@
+
+     
+
+    var x = 10;
+    var y = 20;
+    // test A
+    console.log (x+x+1)
+    console.log(y-y-x);
+    //test B
+    console.log ((x-y)+4)
+    console.log (4 + (y + 5))
+    // test C
+    console.log (x * 2 - 4)
+    console.log (4 - y * 2)
+    //test D
+
+    console.log ((20 - x) / y)
+    console.log (x + 2 / y)
+    //test E
+
+    console.log (((1 - x) * (y / 8)) * 4 * 0)
+    console.log ((x + 3) / x)
+
+
+    alert("Valor de X: " + x);
+    alert("Valor de Y: " + y);  
+
